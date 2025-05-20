fcfdc7075460c8d6d3dfca72d5d15a48df12d4f4
Carlos Eduardo Salles de Sousa <carloseduardo.rss@gmail.com>
2025-05-20 06:53:05 -0300

    Atualiza cobra, ainda não funcional
    Possui parte do comportamento,
    mas há um problema em que o raycast
    de visão está sempre colidindo com o cenário,
    impossibilitando a lógica de funcionar corretamente.

--- Assets/Scripts/Enemies/Snake.cs
+++ Assets/Scripts/Enemies/Snake.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,9 +7,41 @@ using UnityEngine.UIElements;
 public class Snake : MonoBehaviour
 {
     [SerializeField]
-    float viewDistance;
+    float walkSpeed;
     [SerializeField]
+    float runSpeed;
 
+    [SerializeField]
+    float facingDirection;
+
+
+    [SerializeField]
+    float viewRaycastDistance;
+    [SerializeField]
+    float groundRaycastOffsetX;
+    float groundRaycastDistance;
+    Vector3 GroundRaycastPosition
+    {
+        get => transform.position + FacingVector * groundRaycastOffsetX;
+    }
+
+    State currentState;
+    Vector3 FacingVector
+    {
+        get => Vector3.Normalize(Vector3.right * facingDirection);
+    }
+
+    Rigidbody2D rb;
+    GameObject playerInstance;
+
+    enum State { IDLE, CHASE, ATTACK }
+
+    void Awake()
+    {
+        currentState = State.IDLE;
+        rb = GetComponent<Rigidbody2D>();
+        groundRaycastDistance = GetComponent<BoxCollider2D>().bounds.size.y;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +54,97 @@ public class Snake : MonoBehaviour
 
     }
 
+    void FixedUpdate()
+    {
+        RaycastHit2D viewHit = ViewRaycast();
+        bool isHittingGround = IsHittingGround();
+
+        float playerDistance = Mathf.Infinity;
+        bool isHittingWall = false;
+        bool isHittingPlayer = false;
+        if (viewHit)
+        {
+            GameObject gameObject = viewHit.collider.attachedRigidbody.gameObject;
+            Debug.Log(gameObject);
+            if (gameObject.CompareTag("Player"))
+            {
+                playerInstance = gameObject;
+                isHittingPlayer = true;
+            }
+            isHittingWall = !isHittingPlayer;
+        }
+        if (playerInstance != null)
+        {
+            playerDistance = Vector3.Distance(transform.position, playerInstance.transform.position);
+        }
+
+        Debug.Log($"ground: {isHittingGround}");
+        Debug.Log($"wall: {isHittingWall}");
+        Debug.Log($"player: {isHittingPlayer}");
+
+        if (currentState == State.IDLE)
+        {
+            if (isHittingPlayer)
+            {
+                currentState = State.CHASE;
+            }
+            else if (!isHittingGround || (isHittingWall && viewHit.distance < groundRaycastOffsetX))
+            {
+                //FlipX();
+            }
+            else
+            {
+                rb.velocity = FacingVector * walkSpeed;
+            }
+        }
+        else if (currentState == State.CHASE)
+        {
+            if (playerDistance > viewRaycastDistance)
+            {
+                currentState = State.IDLE;
+            }
+            else if (viewHit.distance < viewRaycastDistance / 2)
+            {
+                currentState = State.ATTACK;
+            }
+            else
+            {
+                rb.velocity = FacingVector * runSpeed;
+            }
+        }
+        else if (currentState == State.ATTACK)
+        {
+
+        }
+    }
+
     void OnDrawGizmos()
     {
-        Vector3 pos = transform.position + Vector3.left * 1f;
         Gizmos.color = Color.blue;
-        Gizmos.DrawRay(transform.position, Vector3.left * viewDistance);
-        Gizmos.DrawRay(pos, Vector3.down * 1f);
+        Gizmos.DrawRay(transform.position, FacingVector * viewRaycastDistance);
+        Gizmos.DrawRay(GroundRaycastPosition, Vector3.down * groundRaycastDistance);
+    }
+
+    void FlipX()
+    {
+        facingDirection = -facingDirection;
+
+        Vector3 scale = transform.localScale;
+        scale.x = facingDirection;
+        transform.localScale = scale;
+    }
+    RaycastHit2D ViewRaycast()
+    {
+        Debug.DrawRay(transform.position, FacingVector * viewRaycastDistance, Color.red);
+        var hit = Physics2D.Raycast(Vector2.zero, FacingVector, viewRaycastDistance);
+        Debug.Log($"View: {hit.transform.name}");
+        return hit;
+        
+    }
+
+    bool IsHittingGround() {
+        LayerMask mask = LayerMask.GetMask("Default");
+        var hit = Physics2D.Raycast(GroundRaycastPosition, Vector2.down, groundRaycastDistance, mask);
+        return hit;
     }
 }

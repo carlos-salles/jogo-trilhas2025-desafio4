11bfa0e4437bec0d28c80e43d3bfc5d6ce275c12
Carlos Eduardo Salles de Sousa <carloseduardo.rss@gmail.com>
2025-05-22 09:12:11 -0300

    Adiciona dano ao projétil do estilingue

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,6 +6,8 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField]
     float launchForceMax;
+    [SerializeField]
+    float damage;
     Rigidbody2D rb;
 
     void Awake()
@@ -13,9 +15,19 @@ public class Projectile : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject gameObject = collision.gameObject;
+        if (gameObject.CompareTag("Enemy"))
+        {
+            gameObject.GetComponent<Health>().takeDamage(damage);
+        }
+    }
+
     public void LaunchProjectile(Vector2 launchDirection, float launchIntensity)
     {
         Vector2 force = launchForceMax * launchIntensity * launchDirection;
         rb.AddForce(force, ForceMode2D.Impulse);
     }
+
 }
